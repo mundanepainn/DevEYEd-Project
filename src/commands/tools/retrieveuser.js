6458f2d3204599437157c651e0d4ada6c84a5579
mundanepainn <mundeiinpain@gmail.com>
2023-07-18 15:50:01 +1200

    formated retrieveuser

--- src/commands/tools/retrieveuser.js
+++ src/commands/tools/retrieveuser.js
@@ -2,31 +2,43 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('retrieveuser')
-    .setDescription('Retrieves information about mentioned user')
+    .setName("retrieveuser")
+    .setDescription("Retrieves information about mentioned user")
     .addUserOption((option) =>
       option
-        .setName('user')
-        .setDescription('The username of the mentioned user')
+        .setName("user")
+        .setDescription("The username of the mentioned user")
         .setRequired(false)
     ),
-    async execute(interaction) {
-        const user = interaction.options.getUser('user') || interaction.user;
-        const member = await interaction.guild.members.fetch(user.id);
-        const icon = user.displayAvatarURL();
-        const tag = user.tag;
+  async execute(interaction) {
+    const user = interaction.options.getUser("user") || interaction.user;
+    const member = await interaction.guild.members.fetch(user.id);
+    const icon = user.displayAvatarURL();
+    const tag = user.tag;
 
-        const embed = new EmbedBuilder()
-        .setColor(0x000)
-        .setThumbnail(icon)
-        .setAuthor({ name: tag, iconURL: icon })
-        .addFields({ name: 'Member', value: `${user}`, inline: false })
-        .addFields({ name: 'Roles', value: `${member.roles.cache.map(r => r).join(' ')}`, inline: false})
-        .addFields({ name: 'Joined Server', value: `<t:${parseInt(member.joinedAt / 1000)}:R>`, inline: true})
-        .addFields({ name: 'Joined Discord', value: `<t:${parseInt(user.createdAt / 1000)}:R>`, inline: true})
-        .setFooter({ text: `User ID: ${user.id}`})
-        .setTimestamp()
+    const embed = new EmbedBuilder()
+      .setColor(0x000)
+      .setThumbnail(icon)
+      .setAuthor({ name: tag, iconURL: icon })
+      .addFields({ name: "Member", value: `${user}`, inline: false })
+      .addFields({
+        name: "Roles",
+        value: `${member.roles.cache.map((r) => r).join(" ")}`,
+        inline: false,
+      })
+      .addFields({
+        name: "Joined Server",
+        value: `<t:${parseInt(member.joinedAt / 1000)}:R>`,
+        inline: true,
+      })
+      .addFields({
+        name: "Joined Discord",
+        value: `<t:${parseInt(user.createdAt / 1000)}:R>`,
+        inline: true,
+      })
+      .setFooter({ text: `User ID: ${user.id}` })
+      .setTimestamp();
 
-        await interaction.reply({ embeds: [embed] });
-    }
+    await interaction.reply({ embeds: [embed] });
+  },
 };
