91b8048eeb4bb118e358735ace6956127f1b52df
mundanepainn <mundeiinpain@gmail.com>
2023-07-21 23:17:04 +1200

    added plugins and requirements for music

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const { token } = process.env;
 const fs = require("fs");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
+const { SoundCloudPlugin } = require("@distube/soundcloud");
+const { YtDlpPlugin } = require("@distube/yt-dlp");
 
 const client = new Client({
   intents: [
@@ -9,10 +13,26 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.config = require("./config.json");
+
+client.distube = new DisTube(client, {
+  leaveOnStop: false,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+  plugins: [
+    new SpotifyPlugin({ emitEventsAfterFetching: true }),
+    new SoundCloudPlugin(),
+    new YtDlpPlugin(),
   ],
 });
 
 client.commands = new Collection();
+client.emotes = client.config.emoji;
 client.commandArray = [];
 
 const functionFolders = fs.readdirSync(`./src/functions`);
